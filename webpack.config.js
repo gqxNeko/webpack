604a0c2a2a96b6865661bb944cb437d18262b4c9
Neko <1741493004@qq.com>
2021-07-05 14:16:16 +0800

    update output

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
 module.exports = {
-	entry: './src/js/1.js',
+	// entry: ['./src/js/1.js','./src/js/2.js'],
+	entry: {
+		result: './src/js/1.js',
+		hello: './src/js/2.js',
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		library: 'result',
+		// filename: 'bundle.js',
+		filename: '[name].js',
+		// library: 'result',
 	},
-	mode:'production'
+	mode: 'production',
 }
